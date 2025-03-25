1ed74ee06c8daf14263010b74ad0419d1c13c65a
Fabio-Carvalho <fabio22191@hotmail.com>
2025-03-25 06:39:36 +0000

    up

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -5,11 +5,9 @@ import { useNavigate } from "react-router-dom";
 import { FaUpRightFromSquare, FaFilePdf } from "react-icons/fa6";
 import Loading from "../../components/Loading";
 
-
 const FormTable = () => {
     const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState(false); 
-
+    const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState<any[]>([]);
     const [filteredData, setFilteredData] = useState<any[]>([]);
     const [municipios, setMunicipios] = useState<string[]>([]);
@@ -18,41 +16,31 @@ const FormTable = () => {
         id: "",
         nome: "",
         cargo: "",
-        municipio: ""
+        municipio: "",
+        startDate: "",
+        endDate: ""
     });
-
-    const [isFiltered, setIsFiltered] = useState(false);
 
     const cargos = ["Prefeito", "Secretário de Finanças", "Contador"];
 
     useEffect(() => {
         setIsLoading(true);
-        const getMunicipios = async () => {
-            try {
-                const response = await formService.getMunicipios();
-                setMunicipios(response.municipios || []);
-            } catch (error) {
-                console.error("Failed to get municipios:", error);
-            }
-        };
-        getMunicipios();
+        formService.getMunicipios().then(response => {
+            setMunicipios(response.municipios || []);
+        }).catch(error => console.error("Failed to get municipios:", error));
         setIsLoading(false);
     }, []);
 
     useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await formService.getAllForms();
-                setData(response.forms || []);
-                setFilteredData(response.forms || []);
-            } catch (error) {
-                console.error("Failed to get forms:", error);
-            }
-        };
-        getData();
+        setIsLoading(true);
+        formService.getAllForms().then(response => {
+            setData(response.forms || []);
+            setFilteredData(response.forms || []);
+        }).catch(error => console.error("Failed to get forms:", error));
+        setIsLoading(false);
     }, []);
 
-    const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const handleFilterChange = (e) => {
         setFilters({ ...filters, [e.target.name]: e.target.value });
     };
 
@@ -61,92 +49,42 @@ const FormTable = () => {
             (filters.id ? form.id.toString().includes(filters.id) : true) &&
             (filters.nome ? form.nome.toLowerCase().includes(filters.nome.toLowerCase()) : true) &&
             (filters.cargo ? form.cargo === filters.cargo : true) &&
-            (filters.municipio ? form.municipio === filters.municipio : true)
+            (filters.municipio ? form.municipio === filters.municipio : true) &&
+            (filters.startDate ? new Date(form.created_at) >= new Date(filters.startDate) : true) &&
+            (filters.endDate ? new Date(form.created_at) <= new Date(filters.endDate) : true)
         );
         setFilteredData(filtered);
-        setIsFiltered(true);
     };
 
     const clearFilters = () => {
-        setFilters({ id: "", nome: "", cargo: "", municipio: "" });
+        setFilters({ id: "", nome: "", cargo: "", municipio: "", startDate: "", endDate: "" });
         setFilteredData(data);
-        setIsFiltered(false);
-    };
-
-    const handlePDF = () => {
-        const downloadUrl = "https://drive.google.com/uc?export=download&id=1xJmbkQZHgw8nQvW6rkM9FdeNrYrHD2Vm";
-        const link = document.createElement("a");
-        link.href = downloadUrl;
-        link.setAttribute("download", "Londrina.pdf"); // Nome do arquivo
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
     };
 
     return (
         <Content>
             <div className="header">
-                <div>
-                    <h1>Formulários</h1>
-                </div>
+                <h1>Formulários</h1>
                 <ButtonSettings onClick={() => navigate("/")}>
-                    <FaUpRightFromSquare />
-                    <span style={{ marginLeft: 10 }}>Adicionar</span>
+                    <FaUpRightFromSquare /> <span style={{ marginLeft: 10 }}>Adicionar</span>
                 </ButtonSettings>
             </div>
-
-            {/* Linha de Filtros */}
             <FilterRow>
-                <Input
-                    type="text"
-                    name="id"
-                    value={filters.id}
-                    onChange={handleFilterChange}
-                    placeholder="Filtrar ID"
-                    disabled={isFiltered}
-                />
-                <Input
-                    type="text"
-                    name="nome"
-                    value={filters.nome}
-                    onChange={handleFilterChange}
-                    placeholder="Filtrar Nome"
-                    disabled={isFiltered}
-                />
-                <Select
-                    name="cargo"
-                    value={filters.cargo}
-                    onChange={handleFilterChange}
-                    disabled={isFiltered}
-                >
+                <Input type="text" name="id" value={filters.id} onChange={handleFilterChange} placeholder="Filtrar ID" />
+                <Input type="text" name="nome" value={filters.nome} onChange={handleFilterChange} placeholder="Filtrar Nome" />
+                <Select name="cargo" value={filters.cargo} onChange={handleFilterChange}>
                     <option value="">Todos</option>
-                    {cargos.map((cargo) => (
-                        <option key={cargo} value={cargo}>
-                            {cargo}
-                        </option>
-                    ))}
+                    {cargos.map(cargo => <option key={cargo} value={cargo}>{cargo}</option>)}
                 </Select>
-                <Select
-                    name="municipio"
-                    value={filters.municipio}
-                    onChange={handleFilterChange}
-                    disabled={isFiltered}
-                >
+                <Select name="municipio" value={filters.municipio} onChange={handleFilterChange}>
                     <option value="">Todos</option>
-                    {municipios.map((municipio) => (
-                        <option key={municipio} value={municipio}>
-                            {municipio}
-                        </option>
-                    ))}
+                    {municipios.map(municipio => <option key={municipio} value={municipio}>{municipio}</option>)}
                 </Select>
-                {isFiltered ? (
-                    <ButtonSettings onClick={clearFilters}>Limpar</ButtonSettings>
-                ) : (
-                    <ButtonSettings onClick={applyFilters}>Filtrar</ButtonSettings>
-                )}
+                <Input type="date" name="startDate" value={filters.startDate} onChange={handleFilterChange} />
+                <Input type="date" name="endDate" value={filters.endDate} onChange={handleFilterChange} />
+                <ButtonSettings onClick={applyFilters}>Filtrar</ButtonSettings>
+                <ButtonSettings onClick={clearFilters}>Limpar</ButtonSettings>
             </FilterRow>
-            
-            {/* TAbela */}
             <TableContainer>
                 <StyledTable>
                     <thead>
@@ -157,6 +95,7 @@ const FormTable = () => {
                             <th>Cargo</th>
                             <th>Município</th>
                             <th>Email</th>
+                            <th>Data de Criação</th>
                             <th>Ações</th>
                         </tr>
                     </thead>
@@ -170,23 +109,19 @@ const FormTable = () => {
                                     <td>{form.cargo}</td>
                                     <td>{form.municipio}</td>
                                     <td>{form.email}</td>
-                                    <td><FaFilePdf className="btn-pdf"  onClick={() => handlePDF()} /></td>
+                                    <td>{new Date(form.created_at).toLocaleDateString()}</td>
+                                    <td><FaFilePdf className="btn-pdf" /></td>
                                 </tr>
                             ))
                         ) : (
                             <tr>
-                                <td colSpan={6} style={{ textAlign: "center", padding: "10px" }}>
-                                    Nenhum resultado encontrado.
-                                </td>
+                                <td colSpan={8} style={{ textAlign: "center", padding: "10px" }}>Nenhum resultado encontrado.</td>
                             </tr>
                         )}
                     </tbody>
                 </StyledTable>
             </TableContainer>
-
-            {isLoading && (
-                <Loading />
-            )}
+            {isLoading && <Loading />}
         </Content>
     );
 };
